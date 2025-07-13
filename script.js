6b2469edd1a4b9b5ed5ed26dcf690617de6c3be0
tetuc1t4 <julietta.espinoza@utec.edu.pe>
2025-07-13 16:12:32 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,5 @@
-fetch('data.json')
-  .then(r => r.json())
-  .then(malla => {
-    const cont = document.getElementById('malla');
-    const completados = JSON.parse(localStorage.getItem('completados')) || [];
-
-    malla.forEach(curso => {
-      const d = document.createElement('div');
-      d.className = 'course';
-      d.innerText = `${curso.codigo}`;
-
-      if (completados.includes(curso.codigo)) {
-        d.classList.add('completed');
-      }
-
-      d.onclick = () => {
-        alert(`📖 ${curso.nombre}\n📅 Ciclo: ${curso.ciclo}`);
-      };
-
-      d.ondblclick = () => {
-        if (d.classList.contains('completed')) {
-          d.classList.remove('completed');
-          const index = completados.indexOf(curso.codigo);
-          if (index !== -1) completados.splice(index, 1);
-        } else {
-          d.classList.add('completed');
-          completados.push(curso.codigo);
-        }
-        localStorage.setItem('completados', JSON.stringify(completados));
-      };
-
-      cont.appendChild(d);
-    });
-  });
+function mostrarInfo(elemento) {
+  const activo = document.querySelector('.semestre.activo');
+  if (activo && activo !== elemento) activo.classList.remove('activo');
+  elemento.classList.toggle('activo');
+}
